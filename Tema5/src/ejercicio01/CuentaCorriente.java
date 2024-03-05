5dbf3b4518a53d0c1cc4af0a85b0f92b2bd5dbcc
jpgonzalez <jpgonzalez@106-03.info2>
2024-03-05 13:55:08 +0100

    1 terminado

--- Tema5/src/ejercicio01/CuentaCorriente.java
+++ Tema5/src/ejercicio01/CuentaCorriente.java
@@ -2,44 +2,79 @@ package ejercicio01;
 
 public class CuentaCorriente {
 
-	String dni;
-	String nombre;
-	double saldo;
+	private String dni = "";
+	private String nombre = "";
+	private double saldo;
 
 	public CuentaCorriente(String dni, double saldo) {
-		super();
-		this.dni = dni;
-		this.saldo = saldo;
+		if (dni != null && !dni.equals("")) {
+			this.dni = dni;
+		}
+		if (saldo > 0) {
+			this.saldo = saldo;
+		}
 	}
 
 	public CuentaCorriente(String dni, String nombre, double saldo) {
-		super();
-		this.dni = dni;
-		this.nombre = nombre;
-		this.saldo = saldo;
+		this(dni, saldo);
+		if (nombre != null && !nombre.equals("")) {
+			this.nombre = nombre;
+		}
 	}
-	
+
 	public boolean sacarDinero(double cantidad) {
 		boolean confirmacion = false;
-		
-		if(cantidad >= 0 && cantidad < this.saldo) {
+
+		if (cantidad >= 0 && cantidad <= this.saldo) {
 			confirmacion = true;
 			this.saldo -= cantidad;
 		}
-		
+
 		return confirmacion;
-		
+
 	}
-	
+
 	public void ingresarDinero(double cantidad) {
-		this.saldo += cantidad;
+		if (cantidad > 0) {
+			this.saldo += cantidad;
+		}
 	}
-	
+
 	public void mostrarInfo() {
 		System.out.println("Nombre del propietario: " + this.nombre);
-		System.out.println("DNi del propietario: " + this.dni);
+		System.out.println("Dmi del propietario: " + this.dni);
 		System.out.println("Saldo de la cuenta: " + this.saldo);
 	}
-	
+
+	public String toString() {
+		String cadena = "";
+		cadena += "Nombre del propietario: " + this.nombre + "\n";
+		cadena += "Dni del propietario: " + this.dni + "\n";
+		cadena += "Saldo de la cuenta: " + this.saldo + "\n";
+
+		return cadena;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre != null && !nombre.equals("")) {
+			this.nombre = nombre;
+		}
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getDni() {
+		return dni;
+	}
 
 }
--- Tema5/src/ejercicio01/Main.java
+++ Tema5/src/ejercicio01/Main.java
@@ -3,36 +3,70 @@ package ejercicio01;
 import java.util.Scanner;
 
 public class Main {
-	
-	 static Scanner sc = new Scanner(System.in);
+
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+
+		CuentaCorriente cc = new CuentaCorriente("53962931D", "Pablo", 0);
+
 		int opc;
-		
-		menu();
-		
+
+		boolean salir = false;
+
+		do {
+			menu();
+			System.out.println("Dime que acción quieres realizar (1 - 4)");
+			opc = sc.nextInt();
+
+			switch (opc) {
+			case 1: {
+				System.out.println("Dime cuanto dinero quieres sacar");
+				double sacar = sc.nextDouble();
+				if (sacar < 0) {
+					System.out.println("Cantidad no válida");
+				}
+				cc.sacarDinero(sacar);
+				break;
+			}
+			case 2: {
+				System.out.println("Cuanto dinero quieres ingresar");
+				double ingresar = sc.nextDouble();
+				if (ingresar < 0) {
+					System.out.println("Cantidad no válida");
+				}
+				cc.ingresarDinero(ingresar);
+				break;
+			}
+			case 3: {
+				cc.mostrarInfo();
+				break;
+			}
+			case 4: {
+				System.out.println("Saliendo...");
+				salir = true;
+				break;
+			}
+			default: {
+				System.out.println("Opción no válida");
+				break;
+			}
+			}
+		} while (!salir);
 
 	}
-	
+
 	public static void menu() {
-		
+
 		System.out.println(" - - - - - OPERACIONES - - - - - ");
 		System.out.println("|                               |");
 		System.out.println("|  1. Retirada de dinero        |");
 		System.out.println("|  2. Ingreso de dinero         |");
-		System.out.println("|  3. Informaciónd de la cuenta |");
+		System.out.println("|  3. Información de la cuenta  |");
+		System.out.println("|  4. Salir                     |");
 		System.out.println("|                               |");
 		System.out.println(" - - - - - - - - - - - - - - - - ");
-		
-	}
-	
-	public static void comprobacion(int opc) {
-		
-		do {
-			opc = sc.nextInt();
-		}while(opc < 1 || opc > 3);
-		
+
 	}
 
 }
